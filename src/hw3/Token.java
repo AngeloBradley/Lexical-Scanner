ecbfa762d4fd84bf0ac70cc3d461ffeb3d9a91f7
FalconVortex <abradley818@gmail.com>
2018-03-18 16:45:07 -0400

    Formatting Update

--- src/hw3/Token.java
+++ src/hw3/Token.java
@@ -1,20 +1,17 @@
 /*
  *  Copyright Angelo Bradley 2018
  */
-
-
-
 package hw3;
 
 public class Token {
+
     //Properties
     Type type;
     String value;
-    
-    public Token(String v, Type t){
+
+    public Token(String v, Type t) {
         value = v;
         type = t;
     }
-    
 
 }
--- src/hw3/Tokenizer.java
+++ src/hw3/Tokenizer.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tokenizer {
-    
+
     public Tokenizer() {
 
     }
@@ -70,10 +70,9 @@ public class Tokenizer {
             }
 
         }
-         myData.close();
-         
-         return tokens;
+        myData.close();
+
+        return tokens;
     }
-    
-    
+
 }
